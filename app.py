40794055664f11fe7c368a4b21ead35c75ad1649
Dimacio <dimaciodimacio@gmail.com>
2023-03-18 10:21:24 -0300

    send email

--- app.py
+++ app.py
@@ -2,11 +2,22 @@ from flask import Flask , render_template,  request
 import pandas as pd
 from flask_bootstrap import Bootstrap
 from forms import ContactForm
+from flask_mail import Mail, Message
 import os
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.secret_key = os.urandom(24)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 587,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+   
+}
+
+mail = Mail(app)
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -29,7 +40,10 @@ def contacto():
         subject = request.form["subject"]
         message = request.form["message"]
         res = pd.DataFrame({'name': name, 'email': email, 'subject': subject, 'message': message}, index=[0])
-        res.to_csv('./contactusMessage.csv')
+        #res.to_csv('./contactusMessage.csv')
+        msg = Message('Hello', sender = email, recipients = ['test.mail.protocol.lm@gmail.com'])
+        msg.body = res
+        mail.send(msg)
         return render_template('contacto.html', form=form)
     else:
         return render_template('contacto.html', form=form)
